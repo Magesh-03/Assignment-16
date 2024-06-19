d40e363b1e02736a2547e8b28b9df654cdde3039
Magesh-03 <mageshardik@gmail.com>
2024-06-19 22:53:29 +0530

    .langingpage

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Action from './Components/Action'
+import Footer from './Components/Footer'
+import Head from './Components/Head'
+import Icon from './Components/Icon'
+import Image from './Components/Image'
+import Navbar from './Components/navbar'
+import Testimonial from'./Components/Testimonial'
+
+function App() {
+  return <>
+<Navbar/>
+ <Head/>    
+   <Icon/>
+   <Image/>
+   <Testimonial/>
+    <Action/>
+    <Footer/>  
+    
+  </>
+  
+  
+}
+
+export default App
