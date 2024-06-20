ddbe7947629fde5d6acdf3145e85e1138b24595b
Magesh-03 <mageshardik@gmail.com>
2024-06-20 14:22:58 +0530

    Assignment-16

--- src/Components/Testimonial.jsx
+++ src/Components/Testimonial.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import image4 from './../assets/img/testimonials-1.jpg'
+import image5 from './../assets/img/testimonials-2.jpg'
+import image6 from './../assets/img/testimonials-3.jpg'
 
 function testimonial() {
   return (
@@ -9,21 +12,22 @@ function testimonial() {
             <div className="row">
                 <div className="col-lg-4">
                     <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                        <img className="img-fluid rounded-circle mb-3" src="src/assets/img/testimonials-1.jpg" alt="..." />
+                        <img className="img-fluid rounded-circle mb-3" src="src/assets/img/testimonials-1.jpg" alt="..."  />
                         <h5>Margaret E.</h5>
                         <p className="font-weight-light mb-0">"This is fantastic! Thanks so much guys!"</p>
                     </div>
                 </div>
                 <div className="col-lg-4">
                     <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                        <img className="img-fluid rounded-circle mb-3" src="src/assets/img/testimonials-2.jpg" alt="..." />
+                        <img className="img-fluid rounded-circle mb-3" src="src/assets/img/testimonials-2.jpg" alt="..." /> 
                         <h5>Fred S.</h5>
                         <p className="font-weight-light mb-0">"Bootstrap is amazing. I've been using it to create lots of super nice landing pages."</p>
                     </div>
                 </div>
                 <div className="col-lg-4">
                     <div className="testimonial-item mx-auto mb-5 mb-lg-0">
-                        <img className="img-fluid rounded-circle mb-3" src="src/assets/img/testimonials-3.jpg" alt="..." />
+                        
+                    <img className="img-fluid rounded-circle mb-3" src="src/assets/img/testimonials-3.jpg" alt="..." />
                         <h5>Sarah W.</h5>
                         <p className="font-weight-light mb-0">"Thanks so much for making these free resources available to us!"</p>
                     </div>
